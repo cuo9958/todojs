14d5caf22d73881642029b01e18d82408d12a2aa
guofangchao <guofangchao@daling.com>
2020-01-15 10:36:36 +0800

    自动关闭过期的任务

--- server/services/tasks.js
+++ server/services/tasks.js
@@ -18,17 +18,23 @@ function runTask(ext) {
         const list = await TaskModel.search(day, last_time);
         console.log(day, last_time);
         list.forEach(item => {
-            DDService.msgTo(item.title, item.tell.split(","), item.is_all);
-            if (item.to_count > 1) {
-                Task_list.push({
-                    title: item.title,
-                    tell: item.tell,
-                    count: item.to_count - 1,
-                    is_all: item.is_all
-                });
-            }
+            run_end(item);
         });
     };
+}
+
+function run_end(model) {
+    DDService.msgTo(model.title, model.tell.split(","), model.is_all);
+    if (model.to_count > 1) {
+        Task_list.push({
+            title: model.title,
+            tell: model.tell,
+            count: model.to_count - 1,
+            is_all: model.is_all
+        });
+    } else {
+        TaskModel.update({ status: 0 }, model.id);
+    }
 }
 
 function other() {
@@ -38,6 +44,8 @@ function other() {
         if (item.count > 1) {
             item.count--;
             list.push(item);
+        } else {
+            TaskModel.update({ status: 0 }, item.id);
         }
     });
     Task_list = list;
