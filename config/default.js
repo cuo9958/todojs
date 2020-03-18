52b14cc3acb5ec84a5e356aa644e7836788d7c23
guofangchao <guofangchao@daling.com>
2020-03-18 18:59:48 +0800

    添加新的配置

--- config/default.js
+++ config/default.js
@@ -6,7 +6,7 @@ module.exports = {
     name: "服务",
     //开发环境数据库
     db: {
-        host: "10.14.108.89",
+        host: "l-fe2.dev.bj6.daling.com",
         port: "3306",
         database: "fe_todo",
         user: "fe_todo",
@@ -14,7 +14,7 @@ module.exports = {
         connectionLimit: 2
     },
     //开发环境，普通redis配置
-    redis: "redis://10.14.108.89:6379",
+    redis: "redis://l-fe2.dev.bj6.daling.com:6379",
     dingding: {
         preTxt: "!!",
         url: "https://oapi.dingtalk.com/robot/send?access_token=a1d82d8627a7887cc7586d404f528f1d3151251f21f76961bc896193edf1c731"
